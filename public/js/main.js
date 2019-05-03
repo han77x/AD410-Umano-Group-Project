f992edde4c1ff855c57e86fe145d61586a5e64b3
kyleo83 <kyleostling@gmail.com>
2019-05-02 19:33:50 -0700

    dynamic list builder

--- public/js/main.js
+++ public/js/main.js
@@ -16,7 +16,7 @@ function initMap() {
 }
 /*Converts address to geolocation and centers map on it */
 function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById('address').value;
+  var address = document.getElementById('searchbar').value;
   geocoder.geocode({  'address': address}, function(results, status) {
     if (status === 'OK') {
       resultsMap.setCenter(results[0].geometry.location);
@@ -29,3 +29,47 @@ function geocodeAddress(geocoder, resultsMap) {
     }
   });
 }
+
+$(document).ready(function(){
+	getBuildingType();
+});
+
+//get all messages from db
+function getBuildingType() {
+	$.getJSON("http://localhost:3000/api/v1/addresses/", (result)=>{
+		// buildingTypeList =  result;
+    var unique;
+    var buildingList = [];
+    for (var i = 0; i < result.length; i++) {
+      unique = true;
+      for(var b in buildingList){
+        if(result[i].BuildingType == buildingList[b]) {
+          unique = false;
+          break;
+        }
+      }
+      if(unique == true) {
+        buildingList.push(result[i].BuildingType);
+      }
+    }
+    buildingType(buildingList);
+	});
+}
+
+function buildingType(bl) {
+  var buildingTypeList = []
+  for(i in bl) {
+    buildingTypeList.push("<input type='checkbox' name=checkbox" + i + " value='check_box'>" + bl[i])
+  }
+  var text = "";
+  for(b in buildingTypeList) {
+    text += "<li>" + buildingTypeList[b] + "</li>";
+    document.getElementById("building_type").innerHTML = text;
+  }
+}
+
+
+// function buildingType() {
+//   buildingTypeList = ["Single-Family", "Condo/Townhome", "Multi-Family", "Retail", "Office"];
+//   document.getElementById("build_type").innerHTML = buildingTypeList;
+// }
